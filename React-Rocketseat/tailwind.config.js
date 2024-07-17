07be491809e3fb22fcca7718428ef354de3da0f0
PeterAires <168139612+PeterAires@users.noreply.github.com>
2024-07-16 23:33:03 -0300

    .

--- React-Rocketseat/tailwind.config.js
+++ React-Rocketseat/tailwind.config.js
@@ -8,6 +8,9 @@ export default {
     extend: {
       fontFamily: {
         sans: 'Inter',
+      },
+      boxShadow: {
+        shape: 'px 8px 8px rgba(0,0,0,0.1), 8px 4px 4px rgba(0,0,0,0.1), 8px 2px 2px rgba(0,0,0,0.1), 0px 0px 0px 1px rgba(0,0,0.1), inset 0px 1px 0px rgba(255,255,255,0.03)'
       }
     },
   },
